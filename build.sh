9b40d09494dbcde8bef6d018eedab45b241f190a
Abhisek Mishra <abhisekmishra927@gmail.com>
2026-06-15 13:52:49 +0530

    Auto create admin

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='Abhisek').exists():
+    User.objects.create_superuser('Abhisek','abhisekmishra927@gmail.com','Abhi@1')"
